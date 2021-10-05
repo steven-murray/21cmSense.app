f7aaaef1a27add52bf563f61efc087fd9aef55ec
bpape1usa <79953801+bpape1usa@users.noreply.github.com>
2021-10-04 21:17:19 -0700

    US25-20 Example of calling 21cmSense library from Flask app (#5)
    * Test flask
    
    * Test flask
    
    * Add all 21cmSense reqs
    
    * Test Flask routing
    
    * Add new 21cmSense requirements
    
    * Example call of 21cmSense module
    
    * Document use of 21cmSense as a library
    
    * Add 21cmSense to gitignore
    
    * Add 21cmSense code to travis
    
    * Remove venv activate from travis yml
    
    * cd into 21cm dir
    
    * Make pytest ignore 21cmSense library
    
    * Add trailing slash to pytest ignore dir
    
    * Try a glob ignore
    
    * cd to ~ in travis.yml
    
    * ls in home
    
    * add pwd

---
+++ 21cmSense-library.md
@@ -0,0 +1,9 @@
+# get 21cmSense code
+git clone https://github.com/steven-murray/21cmSense
+
+# activate our virtual env
+source venv/bin/activate
+
+# we now have our virtual env version of python
+# install py21cmsense library egg into our venv site-packages
+python 21cmSense/setup.py install
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from py21cmsense import GaussianBeam, Observatory, Observation, PowerSpectrum, hera
+import numpy as np
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():  # put application's code here
+    return 'Welcome to Project 43!'
+
+
+@app.route("/apireturn")
+def api_return():
+
+    # return a dict as JSON
+    return {
+        "code": "json encoded return",
+    }
+
+@app.route("/21cm")
+def to_cm_if():
+    sensitivity = PowerSpectrum(
+        observation = Observation(
+            observatory = Observatory(
+                antpos = hera(hex_num=7, separation=14, dl=12.12, units="m"),
+                beam = GaussianBeam(frequency=135.0, dish_size=14),
+                latitude=38*np.pi/180.0
+            )
+        )
+    )
+    power_std = sensitivity.calculate_sensitivity_1d()
+    z=zip([v.value for v in sensitivity.k1d], [v.value for v in power_std])
+    return(dict(z))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
