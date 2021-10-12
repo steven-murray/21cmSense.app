3ab8827ac940f721e3c78e2d45c85efaba400da1
lstahlinski <80134802+lstahlinski@users.noreply.github.com>
2021-10-11 21:34:10 -0500

    Initial Web Page Commit

---
+++ web-app-front-end/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <h1>Basic Input Form</h1>
+      <form>
+        <label>
+          Measurement: <input type="text" />
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default App
