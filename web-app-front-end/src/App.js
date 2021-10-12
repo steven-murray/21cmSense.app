73fd64748d7373a2d8153bd3301438c1a53414be
lstahlinski <80134802+lstahlinski@users.noreply.github.com>
2021-10-11 21:46:08 -0500

    Add input field

--- web-app-front-end/src/App.js
+++ web-app-front-end/src/App.js
@@ -1,15 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [firstmeasurment, setFmeasure] = useState("")
+  
+  const handleChange = e => {
+    setFmeasure(e.target.value)
+  }
+
   return (
     <div>
       <h1>Basic Input Form</h1>
       <form>
         <label>
-          Measurement: <input type="text" />
+          Measurement:{" "} 
+          <input type="text" value={firstmeasurment} onChange={handleChange} />
         </label>
       </form>
+      <h5>Measurement: {firstmeasurment}</h5>
     </div>
   )
 }
