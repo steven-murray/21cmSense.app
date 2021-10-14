2a3667c3785bad21f4116aa711ee84958694eeb2
lstahlinski <80134802+lstahlinski@users.noreply.github.com>
2021-10-14 17:01:40 -0500

    Corrected URI

--- web-app-front-end/src/components/Navbar.js
+++ web-app-front-end/src/components/Navbar.js
@@ -42,7 +42,7 @@ function Navbar() {
 			</li>
 			<li className='nav-item'>
 			  <Link
-			    to='/services'
+			    to='/outputdata'
 			    className='nav-links'
 			    onClick={closeMobileMenu}
 			  >
