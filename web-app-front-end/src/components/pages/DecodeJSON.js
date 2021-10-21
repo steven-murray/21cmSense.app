a31dc2c4877ea7300bf472d101c78fdbf9f55579
Edward Kim <elkim1@asu.edu>
2021-10-20 23:00:04 -1000

    Fixed error should work without refreshing the page

--- web-app-front-end/src/components/pages/DecodeJSON.js
+++ web-app-front-end/src/components/pages/DecodeJSON.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import '../../App.css';
 
+
+function DecodeJSON() {
+
 //utilizes the fetch api to get data from testData.json that is located in the 
 //public folder. Not sure how to fix the error in regards to having it in the data folder in src. 
 //Using console.log to log the data from testData and then outputing it into a div id 
-fetch('testData.json')
+	fetch('testData.json')
 	.then(response => response.json())
 	.then(data => {
 		console.log(data.antenna)
-		document.querySelector("#testAntenna").innerText = data.antenna
+		document.getElementById('testAntenna').textContent = data.antenna
 		console.log(data.beam)
-		document.querySelector("#testBeam").innerText = data.beam
+		document.getElementById('testBeam').textContent = data.beam
 		console.log(data.latitude)
-		document.querySelector("#testLatitude").innerText = data.latitude
+		document.getElementById('testLatitude').textContent = data.latitude
 	})
 
-function DecodeJSON() {
+
 	return (
 		<div>
 		<h1>DECODE JSON to display front end</h1>
@@ -26,7 +29,9 @@ function DecodeJSON() {
 	   
 	);
 
+
 	
 }
+
 
 export default DecodeJSON;
