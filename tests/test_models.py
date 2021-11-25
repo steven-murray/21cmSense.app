5a71e6d6ddded02bcf16a11afb5fe01a05a0a895
Brian Pape <bpape1@asu.edu>
2021-11-25 08:42:23 -0800

    test input json validation (118)

--- tests/test_models.py
+++ tests/test_models.py
@@ -123,3 +123,114 @@ def test_hash_variance():
     }"""
     return_json = one_d_cut(input_json)
     assert True
+
+
+# "antenna" group is misisng "schema" keyword
+def test_missing_value():
+    input_json = """{
+      "calculation": "1D-cut-of-2D-sensitivity",
+      "data":{
+        "antenna":{
+          "hex_num": 7,
+          "separation": 14,
+          "dl": 12.02
+        },
+        "beam":{
+          "schema":"GaussianBeam",
+          "frequency": 100,
+          "dish_size": 14
+        },
+        "location":{
+          "schema": "latitude",
+          "latitude": 1.382
+        }
+      },
+      "units":{
+        "antenna":{
+          "hex_num": "m",
+          "separation": "m",
+          "dl": "m"
+        },
+        "beam":{
+          "frequency": "MHz",
+          "dish_size": "m"
+        },
+        "location":{
+          "latitude": "deg"
+        }
+      }
+    }"""
+    return_json = one_d_cut(input_json)
+    assert True
+
+# "antenna" group is missing "hex_num" keyword
+def test_missing_value():
+    input_json = """{
+      "calculation": "1D-cut-of-2D-sensitivity",
+      "data":{
+        "antenna":{
+          "schema": "hera",
+          "separation": 14,
+          "dl": 12.02
+        },
+        "beam":{
+          "schema":"GaussianBeam",
+          "frequency": 100,
+          "dish_size": 14
+        },
+        "location":{
+          "schema": "latitude",
+          "latitude": 1.382
+        }
+      },
+      "units":{
+        "antenna":{
+          "hex_num": "m",
+          "separation": "m",
+          "dl": "m"
+        },
+        "beam":{
+          "frequency": "MHz",
+          "dish_size": "m"
+        },
+        "location":{
+          "latitude": "deg"
+        }
+      }
+    }"""
+    return_json = one_d_cut(input_json)
+    assert True
+
+# "antenna" group is completely missing
+def test_missing_group():
+    input_json = """{
+      "calculation": "1D-cut-of-2D-sensitivity",
+      "data":{
+        "beam":{
+          "schema":"GaussianBeam",
+          "frequency": 100,
+          "dish_size": 14
+        },
+        "location":{
+          "schema": "latitude",
+          "latitude": 1.382
+        }
+      },
+      "units":{
+        "antenna":{
+          "hex_num": "m",
+          "separation": "m",
+          "dl": "m"
+        },
+        "beam":{
+          "frequency": "MHz",
+          "dish_size": "m"
+        },
+        "location":{
+          "latitude": "deg"
+        }
+      }
+    }"""
+    return_json = one_d_cut(input_json)
+    assert True
+
