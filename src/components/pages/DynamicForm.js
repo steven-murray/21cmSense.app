0e625df918af1a8e7ba1b9f9911bbd6d328a744d
hmungalp <hmungalp@asu.edu>
2021-11-29 11:43:16 -0800

    Task#110_hotfix  fixed datatype issue.  Changes to be committed: 	modified:   src/components/pages/DynamicForm.js

--- src/components/pages/DynamicForm.js
+++ src/components/pages/DynamicForm.js
@@ -48,7 +48,7 @@ class DynamicForm extends React.Component {
                                                     "help": "The distance between antennas along a side"
                                                 },
                                                 "dl": {
-                                                    "type": "float",
+                                                    "type": "number",
                                                     "minimum": 0,
                                                     "help": "The distance between rows of antennas"
                                                 },
@@ -60,7 +60,7 @@ class DynamicForm extends React.Component {
                                                           "s"
                                                       ]
                                                   },
-                                                  "dl_unit": {
+                                                  "dl_Unit": {
                                                       "type": "string",
                                                       "default": "m",
                                                       "enum": [
