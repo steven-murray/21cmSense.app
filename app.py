acabeb2226511367c5e7f04b4c3db3566071fa2b
Brian Pape <bpape1@asu.edu>
2022-01-19 22:23:34 -0800

    Update bind address typing

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ import ipaddress
 
 parser = argparse.ArgumentParser(description="Web interface for py21cmSense astronomy software")
 parser.add_argument('--port', default=8080, type=int)
-parser.add_argument('--bind-address', dest="bind_address", default='0.0.0.0', type=ipaddress.IPv4Address)
+parser.add_argument('--bind-address', dest="bind_address_raw", default='0.0.0.0', type=ipaddress.IPv4Address)
 args = parser.parse_args()
 
-app = create_app('default')
-
+bind_address = args.bind_address_raw.exploded
 print("Binding to interface ", args.bind_address)
 print("Starting app on port ", args.port)
 
+app = create_app('default')
+
 if __name__ == '__main__':
-    app.run(host=args.bind_address, port=args.port, debug=True)
+    app.run(host=bind_address, port=args.port, debug=True)
