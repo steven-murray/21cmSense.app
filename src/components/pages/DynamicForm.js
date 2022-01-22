7566be607b13e2b10600fc086995c693263bbd89
Edward Kim <35308036+elkim1@users.noreply.github.com>
2022-01-21 22:43:03 -1000

    Fixed error of not deploying dynamicForm, Task is completed

--- src/components/pages/DynamicForm.js
+++ src/components/pages/DynamicForm.js
@@ -64,68 +64,50 @@ class DynamicForm extends React.Component {
 						Example 2  <br></br>
 						Example 3  <br></br>
 						Example 4  <br></br>
-						Example 5  <br></br>
-						Example 6  <br></br>
-						Example 7  <br></br>
-						Example 8  <br></br>
+
 						<br></br>
-                 <p> <button><h5> PLOT </h5> </button> </p>                  
+                 <p> <button><h5> PLOT </h5> </button> </p>
             </form>
           </div>
           <div>
             	<br></br>
                 <div class="row">
-                    <div>  <DropDown options={calc}/> 	
+                    <div>  <DropDown options={calc}/>
 						   <br></br><br></br>
  							<div class="row">
-                    			<div> <p><h6>Antenna Information</h6></p> </div>                    
+                    			<div> <p><h6>Antenna Information</h6></p> </div>
                 			</div>
                 			<div class="row">
-                    			<div>   
-									<label> Hex Number </label>            
-                   					<input type = {ant.data.antenna.hex_num.type} min = {ant.data.antenna.hex_num.minimum} placeholder = {ant.data.antenna.hex_num.help}/>
-								</div> 								 
-							</div>
-							<br></br>
-							<div class="row">
-                    			<div>   
-									<label> Separation </label>            
-                   					<input type = {ant.data.antenna.separation.type} min = {ant.data.antenna.separation.minimum} placeholder = {ant.data.antenna.separation.help}/>
-									<DropDown type = {ant.units.antenna.separation.type} default = {ant.units.antenna.separation.default} options={ant.units.antenna.separation.enum}/>
-								</div>  
-							</div>	
-							<br></br>
-							<div class="row">
-                    			<div>   
-									<label> Frequency </label>            
-                   					<input type = {ant.data.antenna.frequency.type} min = {ant.data.antenna.frequency.minimum} placeholder = {ant.data.antenna.frequency.help}/>
-									<DropDown type = {ant.units.antenna.frequency.type} default = {ant.units.antenna.frequency.default} options={ant.units.antenna.frequency.enum} required/>
-								</div>  
-							</div>	
-							<br></br>
-							<div class="row">
-                    			<div>   
-									<label> Dish Size </label>            
-                   					<input type = {ant.data.antenna.dish_size.type} min = {ant.data.antenna.dish_size.minimum} placeholder = {ant.data.antenna.dish_size.help}/>
-									<DropDown type = {ant.units.antenna.dish_size.type} default = {ant.units.antenna.dish_size.default} options={ant.units.antenna.dish_size.enum} required/>
-								</div>  
-							</div>	
-							<br></br>
-							<div class="row">
-                    			<div>   
-									<label> Distance </label>            
-                   					<input type = {ant.data.antenna.dl.type} min = {ant.data.antenna.dl.minimum} placeholder = {ant.data.antenna.dl.help}/>
-									<DropDown type = {ant.units.antenna.dl.type} default = {ant.units.antenna.dl.default} options={ant.units.antenna.dl.enum}/>
-								</div>  
-							</div>
-							<div className="row">
-								<div>
-									<label> Latitude </label>
-									<input type={ant.data.antenna.latitude.dl.type} min={ant.data.antenna.latitude.dl.minimum} placeholder={ant.data.antenna.latitude.dl.help}/>
-									<DropDown type={ant.units.antenna.latitude.dl.type} default={ant.units.antenna.latitude.dl.default} options={ant.units.antenna.latitude.dl.enum}required/>
+                    			<div>
+									<label> Hex Number </label>
+                   					<input type = {ant.data.antenna.hex_num.type} min = {ant.data.antenna.hex_num.minimum} placeholder = {ant.data.antenna.hex_num.help} required/>
 								</div>
 							</div>
-					</div>
+							<br></br>
+							<div class="row">
+                    			<div>
+									<label> Separation </label>
+                   					<input type = {ant.data.antenna.separation.type} min = {ant.data.antenna.separation.minimum} placeholder = {ant.data.antenna.separation.help} required/>
+									<DropDown type = {ant.units.antenna.separation.type} default = {ant.units.antenna.separation.default} options={ant.units.antenna.separation.enum}/>
+								</div>
+							</div>
+							<br></br>
+							<div class="row">
+                    			<div>
+									<label> Distance </label>
+                   					<input type = {ant.data.antenna.dl.type} min = {ant.data.antenna.dl.minimum} placeholder = {ant.data.antenna.dl.help} required/>
+									<DropDown type = {ant.units.antenna.dl.type} default = {ant.units.antenna.dl.default} options={ant.units.antenna.dl.enum}/>
+								</div>
+							</div>
+                        <br></br>
+                        <div className="row">
+                            <div>
+                                <label> Latitude </label>
+                                <input type={ant.data.antenna.dl.type} min={ant.data.antenna.dl.minimum} placeholder={ant.data.antenna.dl.help} required/>
+                                <DropDown type={ant.units.antenna.dl.type} default={ant.units.antenna.dl.default} options={ant.units.antenna.dl.enum}/>
+                            </div>
+                        </div>
+				    </div>
                     <div> <h6>CALCULATE</h6>   </div>
                 </div>			
           </div>
