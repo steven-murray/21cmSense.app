794b2211da50878a77fef80b35e4f9fe83e9a44d
hmungalp <hmungalp@asu.edu>
2022-01-29 08:36:56 -0800

    Added MODELS lable in the models panel  Changes to be committed: 	modified:   src/components/pages/the21cmSense.js

--- src/components/pages/the21cmSense.js
+++ src/components/pages/the21cmSense.js
@@ -10,6 +10,7 @@ function the21cmSense(){
     }}>
       <br></br>
       <Panel  shaded >
+		<label style={{fontWeight: 'bold', fontSize:24, fontFamily: 'Times New Roman'}}> Model </label>
 		<button style={{ float: 'right', fontWeight: 'bold', fontSize:18}} title="New Model"> + </button> 
 		<br></br><br></br>
         No models created yet. Please click "New Model"
