9754a8ce244f6bd821bc77a39d69ac88a7273ee7
hmungalp <hmungalp@asu.edu>
2022-01-31 10:15:23 -0800

    Linked createModel to Button click  Changes to be committed: 	modified:   src/App.js 	modified:   src/components/pages/createModel.js 	modified:   src/components/pages/the21cmSense.js

---
+++ src/components/pages/createModel.js
@@ -0,0 +1,15 @@
+import '../../App.css';
+import React from 'react';
+
+class CreateModel extends React.Component {
+
+render() {
+     return (
+       
+      <div> LINKED PAGE CREATE MODEL to BUTTON CLICK</div>
+       
+  );
+  }
+}
+	
+export default CreateModel;	
