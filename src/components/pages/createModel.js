9995002e991fa4b57f759faa2a8ef9115cda5db9
hmungalp <hmungalp@asu.edu>
2022-02-01 16:47:49 -0800

    Created panel for Antenna section  Changes to be committed: 	modified:   src/components/pages/createModel.js

--- src/components/pages/createModel.js
+++ src/components/pages/createModel.js
@@ -1,12 +1,17 @@
 import '../../App.css';
 import React from 'react';
+import { Panel } from 'rsuite';
 
 class CreateModel extends React.Component {
 
 render() {
      return (
-       
-      <div> LINKED PAGE CREATE MODEL to BUTTON CLICK</div>
+		<div style={{display: 'block', width: 700, paddingLeft: 30 }}>
+			<br></br>
+      		<Panel  shaded >
+				<label style={{fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell'}}> ANTENNA </label>
+			</Panel>
+ 		</div>
        
   );
   }
