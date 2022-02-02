562a236c4b765ff9d6e3ed793c62997cfbd936bc
hmungalp <hmungalp@asu.edu>
2022-02-02 08:19:04 -0800

    Changed font color and added panel header  Changes to be committed: 	modified:   src/components/pages/createModel.js

--- src/components/pages/createModel.js
+++ src/components/pages/createModel.js
@@ -8,8 +8,7 @@ render() {
      return (
 		<div style={{display: 'block', width: 700, paddingLeft: 30 }}>
 			<br></br>
-      		<Panel  shaded >
-				<label style={{fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell'}}> ANTENNA </label>
+      		<Panel header = 'ANTENNA' shaded style={{color: 'rgb(106, 120, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 30}}>
 			</Panel>
  		</div>
        
