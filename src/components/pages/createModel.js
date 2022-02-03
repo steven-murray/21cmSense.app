63ac17ab0a44b7a010f18b6b0b517060f0e92f00
hmungalp <hmungalp@asu.edu>
2022-02-03 09:55:39 -0800

    Added BEAM panel to createModel page. Changes to be committed: 	modified:   src/components/pages/createModel.js

--- src/components/pages/createModel.js
+++ src/components/pages/createModel.js
@@ -8,7 +8,9 @@ render() {
      return (
 		<div style={{display: 'block', width: 700, paddingLeft: 30 }}>
 			<br></br>
-      		<Panel header = 'ANTENNA' shaded style={{color: 'rgb(106, 120, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 30}}>
+      		<Panel header = 'ANTENNA' shaded style={{color: 'rgb(106, 120, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
+			</Panel>
+			<Panel header = 'BEAM' shaded style={{color: 'rgb(106, 120, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
 			</Panel>
  		</div>
        
