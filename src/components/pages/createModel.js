25454745286300f181562b37ddb729c56b26a527
hmungalp <hmungalp@asu.edu>
2022-02-06 18:52:10 -0800

    Created Label and Input for Hex Number  Changes to be committed: 	modified:   src/components/pages/createModel.js

--- src/components/pages/createModel.js
+++ src/components/pages/createModel.js
@@ -9,6 +9,8 @@ render() {
 		<div style={{display: 'block', width: 700, paddingLeft: 30 }}>
 			<br></br>
       		<Panel header = 'ANTENNA' shaded style={{color: 'rgb(106, 120, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
+				<label> Hex Number </label>            
+                <input required/>
 			</Panel>
 			<Panel header = 'BEAM' shaded style={{color: 'rgb(106, 120, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
 			</Panel>
