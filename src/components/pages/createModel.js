713cdf708285fa6239bdc8c6c4b6213131e70922
hmungalp <hmungalp@asu.edu>
2022-02-07 20:32:28 -0800

    Added label,input, and dropdown for Dish size  Changes to be committed: 	modified:   src/components/pages/createModel.js

--- src/components/pages/createModel.js
+++ src/components/pages/createModel.js
@@ -44,7 +44,11 @@ render() {
 				<DropDown options={option.separation.enum}/>      
   				<br></br><br></br>
 			</Panel>
-			<Panel header = 'BEAM' shaded style={{color: 'rgb(106, 120, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
+			<Panel header = 'BEAM' shaded  style={{color: 'rgb(77, 77, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
+				<label> Dish Size </label>           
+                <input type = {"number"} min={0}required/>
+				<DropDown options={option.separation.enum}/>      
+  				<br></br><br></br>
 			</Panel>
 			<Panel header = 'LOCATION' shaded style={{color: 'rgb(77, 77, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
 				<label> Latitude </label> 
