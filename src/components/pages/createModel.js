f0e1936140e8c4aaccd4ae93fd1a05b982fed7e6
hmungalp <hmungalp@asu.edu>
2022-02-07 20:07:00 -0800

    Added Label, input, and dropdown for Distance field  Changes to be committed: 	modified:   src/components/pages/createModel.js

--- src/components/pages/createModel.js
+++ src/components/pages/createModel.js
@@ -39,6 +39,10 @@ render() {
                 <input required/>
 				<DropDown options={option.separation.enum}/>      
   				<br></br><br></br>
+				<label> Distance </label>           
+                <input required/>
+				<DropDown options={option.separation.enum}/>      
+  				<br></br><br></br>
 			</Panel>
 			<Panel header = 'BEAM' shaded style={{color: 'rgb(106, 120, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
 			</Panel>
