c21a44ff1928bf83a4b818dd78b195cbd141c12f
hmungalp <hmungalp@asu.edu>
2022-02-08 09:43:20 -0800

    Added lable and input for Model Name  Changes to be committed: 	modified:   src/components/pages/createModel.js

--- src/components/pages/createModel.js
+++ src/components/pages/createModel.js
@@ -31,7 +31,7 @@ render() {
 			<div style={{display: 'block', width: 900, paddingLeft: 30 }}>
 			<br></br>
 			
-      		<Panel header = 'ANTENNA' shaded style={{color: 'rgb(77, 77, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
+      		<Panel header = 'ANTENNA' shaded style={{color: 'rgb(77, 77, 58)', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
 				<label> Hex Number </label>            
                 <input type = {"number"} min={3}required/>
 				<br></br><br></br>
@@ -44,18 +44,21 @@ render() {
 				<DropDown options={option.separation.enum}/>      
   				<br></br><br></br>
 			</Panel>
-			<Panel header = 'BEAM' shaded  style={{color: 'rgb(77, 77, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
+			<Panel header = 'BEAM' shaded  style={{color: 'rgb(77, 77, 58)', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
 				<label> Dish Size </label>           
                 <input type = {"number"} min={0}required/>
 				<DropDown options={option.separation.enum}/>      
   				<br></br><br></br>
 			</Panel>
-			<Panel header = 'LOCATION' shaded style={{color: 'rgb(77, 77, 58)', fontWeight: 'bold', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
+			<Panel header = 'LOCATION' shaded style={{color: 'rgb(77, 77, 58)', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
 				<label> Latitude </label> 
 				<input type = {"number"} min={-180} max = {180} required/>
 				<DropDown options={option.separation.enum}/>      
   				<br></br><br></br>
 			</Panel>
+			<br></br><br></br>
+			<label style = {{color: 'rgb(128, 0, 0)', fontSize:18, fontFamily: 'Rockwell', width:180}}> Model Name </label>
+			<input type = {"text"} required/>
  		</div>
   );
   }
