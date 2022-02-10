05c633c6cd56b53d78b18709db81bebedabef8bd
lstahlinski <80134802+lstahlinski@users.noreply.github.com>
2022-02-10 14:00:57 -0600

    Fixing plot download

--- src/components/pages/The21cmSense.js
+++ src/components/pages/The21cmSense.js
@@ -1,53 +1,63 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../../App.css';
 import { Panel } from 'rsuite';
 import '../rsuite-default.css';
-import Select from 'react-select';
 import { GiInfo } from "react-icons/gi";
 import { Link } from 'react-router-dom';
-//import Plot from "react-plotly.js";
+import * as Plotly from 'plotly.js';
+import '../../TestGraphDownload.js';
 // import exportFromJSON from 'export-from-json' 
 //import { Dropdown } from 'react-native-material-dropdown';
 //import {CSVLink, CSVDownload} from 'react-csv';
 
-function The21cmSense(){
+const imagePlot = [
+  Plotly.downloadImage('TestGraphDownload.js', {format: 'png', width: 800, height: 600, filename: 'newplot'})
+]
 
-    //   <Plot
-    //   data={[
-    //     {
-    //       x: [1, 2, 3],
-    //       y: [2, 6, 3],
-    //       type: 'scatter',
-    //       mode: 'lines+markers',
-    //       marker: {color: 'red'},
-    //     },
-    //     {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-    //   ]}
-    //   layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-    // />
+// const data = [
+//   {
+//     label: "Download Image of Plot",
+    
+//     // downloadGraph(fileName) {
+//     //   if(this.graphPlotted) {
+//     //     Plotly.downloadGraph(this.graphPlotted, {format: 'png', filename: fileName})
+//     //   }
+//   },
+//   {
+//     label: "Download JSON Data",
+//     //exportFromJSON({ data: JSONdata, fileName: 'download', exportType: exportFromJSON.types.xls })
+//   },
+//   {
+//     label:"Export Plot Details to CSV"
+//     //<CSVLink data={csvData} >Download me</CSVLink>
+//   }
+// ];
 
-    const data = [
+
+const DropDown = ({ selectedValue, options, onChange }) => {
+  return (
+    <select onChange={onChange} >
       {
-        label: "Download Image of Plot",
+        options.map(o => <data value={o} selected={o === selectedValue}>{o}</data>)
         
-        // downloadGraph(fileName) {
-        //   if(this.graphPlotted) {
-        //     Plotly.downloadGraph(this.graphPlotted, {format: 'png', filename: fileName})
-        //   }
-      },
-      {
-        label: "Download JSON Data",
-        //exportFromJSON({ data: JSONdata, fileName: 'download', exportType: exportFromJSON.types.xls })
-      },
-      {
-        label:"Export Plot Details to CSV"
-        //<CSVLink data={csvData} >Download me</CSVLink>
       }
-    ];
-    const [UseSelectedOption, UseSetSelectedOption] = useState(null);
-    const handleChange = e => {
-      UseSetSelectedOption(e);
-    }
+    </select>
+  );
+}
+
+class The21cmSense extends React.Component{
+    render() {
+      const data = [ {
+        label: "Download Image Of Plot",
+        imagePlot
+      }, {
+        label: "Download JSON Data",
+      }, {
+        label: "Export Plot Details to CSV"
+      }
+      ]
+
+      
     return (
         <div>
             <div style={{
@@ -65,22 +75,8 @@ function The21cmSense(){
               <br></br>
               <Panel  shaded >
               <label style={{fontWeight: 'bold', fontSize:24, fontFamily: 'Times New Roman'}}> Download </label>
-                <Select
-                  placeholder="Select Options"
-                  value={UseSelectedOption}
-                  options={data}
-                  onChange={handleChange}
-                />
 
-                {UseSelectedOption
-                && <div style={{ marginTop: 75, lineHeight: '25px' }}>
-                 <div style={{ marginTop: 10 }}><b>Label: </b> {UseSelectedOption.label}</div>
-                 {/* <Dropdown
-                //   label="Download"
-                //   data={data}
-                //   /> */}
-                  </div>
-                  }
+                  <DropDown options={data.separation.enum}/>
             </Panel>
             </div>
 
@@ -93,5 +89,6 @@ function The21cmSense(){
         </div>
 
     );
+    }
 }
 export default The21cmSense
