06423b856d9feffa109ca3d4d6f6e37fc638bcfb
hmungalp <hmungalp@asu.edu>
2022-02-10 10:07:12 -0800

    removed unused code  Changes to be committed: 	modified:   src/components/pages/createModel.js

--- src/components/pages/createModel.js
+++ src/components/pages/createModel.js
@@ -44,12 +44,7 @@ Button.defaultProps = {
 
 };
 
-function clickMe() {
-  alert("You clicked me!");
-}
-
 class CreateModel extends React.Component {
-
 
 render() {
 		const option = {
@@ -94,8 +89,8 @@ render() {
 			<br></br><br></br>
 			<label style = {{color: 'rgb(128, 0, 0)',  fontSize:18, fontFamily: 'Rockwell', width:180}}> Model Name </label>
 			<input type = {"text"} required/>
-			<Button onclick={clickMe} style = {{fontSize:24, fontFamily: 'Rockwell', width:100}}> Cancel </Button>
-			<Button onclick={clickMe} style = {{fontSize:24, fontFamily: 'Rockwell', width:100}}> Save </Button>
+			<Button style = {{fontSize:24, fontFamily: 'Rockwell', width:100}}> Cancel </Button>
+			<Button style = {{fontSize:24, fontFamily: 'Rockwell', width:100}}> Save </Button>
 		</div>
   );
   }
