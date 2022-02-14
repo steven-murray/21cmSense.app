536c167eab124af9c044e27c9bc8e4663401dd0d
lstahlinski <80134802+lstahlinski@users.noreply.github.com>
2022-02-14 13:47:30 -0600

    Fixed buttons

--- src/components/pages/The21cmSense.js
+++ src/components/pages/The21cmSense.js
@@ -6,58 +6,128 @@ import { GiInfo } from "react-icons/gi";
 import { Link } from 'react-router-dom';
 import * as Plotly from 'plotly.js';
 import '../../TestGraphDownload.js';
-// import exportFromJSON from 'export-from-json' 
+import { saveAs } from "file-saver";
+//import { Button } from '../Button.js'; 
+//import exportFromJSON from 'export-from-json';
+import styled from "styled-components";
 //import { Dropdown } from 'react-native-material-dropdown';
 //import {CSVLink, CSVDownload} from 'react-csv';
 
-const imagePlot = [
-  Plotly.downloadImage('TestGraphDownload.js', {format: 'png', width: 800, height: 600, filename: 'newplot'})
-]
 
-// const data = [
-//   {
-//     label: "Download Image of Plot",
-    
-//     // downloadGraph(fileName) {
-//     //   if(this.graphPlotted) {
-//     //     Plotly.downloadGraph(this.graphPlotted, {format: 'png', filename: fileName})
-//     //   }
-//   },
-//   {
-//     label: "Download JSON Data",
-//     //exportFromJSON({ data: JSONdata, fileName: 'download', exportType: exportFromJSON.types.xls })
-//   },
-//   {
-//     label:"Export Plot Details to CSV"
-//     //<CSVLink data={csvData} >Download me</CSVLink>
-//   }
-// ];
+   //   <Plot
+    //   data={[
+    //     {
+    //       x: [1, 2, 3],
+    //       y: [2, 6, 3],
+    //       type: 'scatter',
+    //       mode: 'lines+markers',
+    //       marker: {color: 'red'},
+    //     },
+    //     {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+    //   ]}
+    //   layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+    // />
 
-
-const DropDown = ({ selectedValue, options, onChange }) => {
-  return (
-    <select onChange={onChange} >
-      {
-        options.map(o => <data value={o} selected={o === selectedValue}>{o}</data>)
+    // const data = [
+    //   {
+    //     label: "Download Image of Plot",
         
-      }
-    </select>
+    //     // downloadGraph(fileName) {
+    //     //   if(this.graphPlotted) {
+    //     //     Plotly.downloadGraph(this.graphPlotted, {format: 'png', filename: fileName})
+    //     //   }
+    //   },
+    //   {
+    //     label: "Download JSON Data",
+    //     //exportFromJSON({ data: JSONdata, fileName: 'download', exportType: exportFromJSON.types.xls })
+    //   },
+    //   {
+    //     label:"Export Plot Details to CSV"
+    //     //<CSVLink data={csvData} >Download me</CSVLink>
+    //   }
+    // ];
+    // const [UseSelectedOption, UseSetSelectedOption] = useState(null);
+    // const handleChange = e => {
+    //   UseSetSelectedOption(e);
+    // }
+// const DropDown = ({ selectedValue, options, onChange }) => {
+//   return (
+//     <select onChange={onChange} >
+//       {
+//         options.map(o => <option value={o} selected={o === selectedValue}>{o}</option>)
+        
+//       }
+//     </select>
+//   );
+// }
+
+
+const theme = {
+  cyan: {
+    default: "#F0FFFF",
+    hover: "#00FFFF"
+  }
+};
+
+const Button = styled.button`
+  background-color: ${(props) => theme[props.theme].default};
+  color: rgb(128, 0, 0);
+  padding: 5px 15px;
+  border-radius: 9px;
+  &:hover {
+    background-color: ${(props) => theme[props.theme].hover};
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
+
+Button.defaultProps = {
+  theme: "cyan"
+
+};
+
+const saveImage = () => {
+  saveAs(
+    "",
+    "example.png"
   );
-}
+};
 
-class The21cmSense extends React.Component{
-    render() {
-      const data = [ {
-        label: "Download Image Of Plot",
-        imagePlot
-      }, {
-        label: "Download JSON Data",
-      }, {
-        label: "Export Plot Details to CSV"
-      }
-      ]
+const saveJSON = () => {
+  saveAs(
+    "",
+    "example.txt"
+  );
+};
 
-      
+const saveCSV = () => {
+  saveAs(
+    "",
+    "example.csv"
+  );
+};
+
+class The21cmSense extends React.Component {
+
+  onButtonClick() {
+    // Do anything
+    this.popupHandler("Update");
+    console.log('Button clicked');
+  }
+  render() {
+    // const option = {
+    //   "download": {
+    //            "type": "string",
+    //            "default": "Please Select Download Option",
+    //            "dropdown": [
+    //              "Image of Graph",
+    //              "Parameters in JSON",
+    //              "Download Graph Data in CSV"
+    //            ]
+    //          },
+    //  }
     return (
         <div>
             <div style={{
@@ -74,9 +144,28 @@ class The21cmSense extends React.Component{
               </Panel>
               <br></br>
               <Panel  shaded >
-              <label style={{fontWeight: 'bold', fontSize:24, fontFamily: 'Times New Roman'}}> Download </label>
+              <label style={{fontWeight: 'bold', fontSize:24, fontFamily: 'Times New Roman'}}> Download Data</label>
+              <br></br><br></br>
+              {/* <DropDown options={option.download.dropdown}/>      */}
+              {/* <Button
+                onClicked={this.onButtonClick()}
+              />  */}
+              {/* <div>
+                <button onClick={saveFile}>Download Image of Graph</button>
+              </div>
+              <div>
+                <button onClick={saveFile}>Download Graph Data in CSV</button>
+              </div> */}
+        
+                <Button onClick={saveJSON} style = {{fontSize:12, fontFamily: 'Rockwell', width:100}}>Download Parameters in JSON</Button>
+           
+                <Button onClick={saveImage} style = {{fontSize:12, fontFamily: 'Rockwell', width:100}}>Download Image of Graph</Button>
+                
+                <Button onClick={saveCSV} style = {{fontSize:12, fontFamily: 'Rockwell', width:100}}>Download Graph Data in CSV</Button>
+             
 
-                  <DropDown options={data.separation.enum}/>
+  				    <br></br><br></br><br></br>
+              
             </Panel>
             </div>
 
@@ -89,6 +178,7 @@ class The21cmSense extends React.Component{
         </div>
 
     );
-    }
+  }
 }
 export default The21cmSense
+ 
