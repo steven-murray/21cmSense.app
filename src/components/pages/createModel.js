a3aec23b701807137abe76653fb24a9d608ae8c4
hmungalp <hmungalp@asu.edu>
2022-03-13 20:57:21 -0700

    Added code to store the model as given template Changes to be committed: 	modified:   src/components/pages/createModel.js

--- src/components/pages/createModel.js
+++ src/components/pages/createModel.js
@@ -5,17 +5,6 @@ import styled from "styled-components";
 import { withCookies, Cookies } from "react-cookie";
 import { instanceOf } from "prop-types";
 
-const DropDown = ({ selectedValue, options, onChange }) => {
-  return (
-    <select onChange={onChange} >
-      {
-        options.map(o => <option value={o} selected={o === selectedValue}>{o}</option>)
-        
-      }
-    </select>
-  );
-}
-
 const theme = {
   blue: {
     default: "#9facde",
@@ -72,9 +61,8 @@ class CreateModel extends React.Component {
 			Location: [],			
 			DataisLoaded: false,
 			user: this.props.cookies.get("user") || ""
-			
+	
 	    }
-
 	  }
 	
 	 componentDidMount(){	
@@ -101,20 +89,53 @@ class CreateModel extends React.Component {
 	}
 	
 	generateModel(uid){
+		
+		const ml = {
+					  "modelname": this.state.modelName,
+					  "data":{
+							  "calculation": "1D-cut-of-2D-sensitivity",
+							  "data":{
+							    "antenna":{
+							      "schema": "hera",
+							      "hex_num": this.state.HexNumber,
+							      "separation": this.state.Separation,
+							      "dl": 12.02
+							    },
+							    "beam":{
+							      "schema":"GaussianBeam",
+							      "frequency": this.state.Frequency,
+							      "dish_size": this.state.DishSize
+							    },
+							    "location":{
+							      "schema": "latitude",
+							      "latitude": this.state.Latitude
+							    }
+							  },
+							  "units":{
+							    "antenna":{
+							      "hex_num": "m",
+							      "separation": this.state.SeperationUnits,
+							      "dl": "m"
+							    },
+							    "beam":{
+							      "frequency": this.state.FrequencyUnits,
+							      "dish_size": "m"
+							    },
+							    "location":{
+							      "latitude": this.state.LatitudeUnits
+							    }
+							  }
+							}
+					}
+		
 		const requestmodel = {
 	        method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-					  modelname: this.state.modelName,
-					  data: {"calculation": "1D-cut-of-2D-sensitivity"}
-					})
-		};
-
-	    fetch('http://galileo.sese.asu.edu:8081/api-1.0/users/'+uid+'/models', requestmodel)
-	  
-	       
-		
-	}
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(ml)
+			};
+	
+		    fetch('http://galileo.sese.asu.edu:8081/api-1.0/users/'+uid+'/models', requestmodel)
+		}
 	
 	getAntennaData(){
             fetch("http://galileo.sese.asu.edu:8081/api-1.0/schema/antenna/get/hera")
@@ -148,12 +169,11 @@ class CreateModel extends React.Component {
 	}
 		
 	handleOnSubmit = (event) => {
-	    event.preventDefault();
-	    this.props.history.push({
+	    event.preventDefault();	
+			    this.props.history.push({
 	      pathname: '/The21cmSense',
 	      state : this.state
-	    });
-		
+	    });	
 		this.generateModel(this.state.user);
 	  };
 	
@@ -164,7 +184,7 @@ class CreateModel extends React.Component {
 			      [name]: value
 			    }));
 		};
-
+	
 render() {
 		const { user } = this.state;
 		const { Antenna,  Beam,Location, DataisLoaded} = this.state;      
@@ -172,7 +192,7 @@ render() {
 		if (!DataisLoaded) return <div>
 			<h1> Please wait some time.... </h1> </div> ;
 	
-     return (
+	  return (
 	
 		 <div style={{display: 'block', width: 900, paddingLeft: 30 }}>
 			<br></br><p>{user} </p> <br></br>
@@ -183,24 +203,32 @@ render() {
 				<br></br><br></br>
 				<label> Separation </label>           
                 <input name = "Separation" type = {Antenna.data.antenna.separation.type} min = {Antenna.data.antenna.separation.minimum}  onChange={this.handleInputChange}   required/>
-				<DropDown name = "SeperationUnits" type = {Antenna.units.antenna.separation.type} defaultValue = {Antenna.units.antenna.separation.default} options={Antenna.units.antenna.separation.enum}  onChange={this.handleInputChange}  />      
-  				<br></br><br></br>
+				<select name = "SeperationUnits" value={Antenna.units.antenna.separation.default} onChange={this.handleInputChange} >
+			      {Antenna.units.antenna.separation.enum.map(o => <option value={o.value}>{o}</option>)}
+			    </select>		
+				<br></br><br></br>
 			</Panel>
 			<Panel header = 'BEAM' shaded  style={{color: 'rgb(77, 77, 58)', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
 				<label> Dish Size </label>           
                 <input name = "DishSize" type = {Beam.data.beam.dish_size.type} min={Beam.data.beam.dish_size.minimum}   onChange={this.handleInputChange}  required/>
-				<DropDown name = "DishSizeUnits"  type = {Beam.units.beam.dish_size.type}  options={Beam.units.beam.dish_size.enum}  />      
-  				<br></br><br></br>
+				<select name = "DishSizeUnits" value={Beam.units.beam.dish_size.default} onChange={this.handleInputChange} >
+			      {Beam.units.beam.dish_size.enum.map(o => <option value={o.value}>{o}</option>)}
+			    </select>		
+				<br></br><br></br>
 				<label> Frequency </label>           
                 <input name = "Frequency" type = {Beam.data.beam.frequency.type} min={Beam.data.beam.frequency.minimum}  onChange={this.handleInputChange} required/>
-				<DropDown name = "FrequencyUnits"   type = {Beam.units.beam.frequency.type}  options={Beam.units.beam.frequency.enum}/>      
-  				<br></br><br></br>
+				<select name = "FrequencyUnits" value={Beam.units.beam.frequency.default} onChange={this.handleInputChange} >
+			      {Beam.units.beam.frequency.enum.map(o => <option value={o.value}>{o}</option>)}
+			    </select>
+				<br></br><br></br>
 			</Panel>
 			<Panel header = 'LOCATION' shaded style={{color: 'rgb(77, 77, 58)', fontSize:21, fontFamily: 'Rockwell', paddingLeft: 20}}>
 				<label> Latitude </label> 
 				<input name = "Latitude"  type = {Location.data.location.latitude.type} min={Location.data.location.latitude.__minimum} max = {Location.data.location.latitude.__maximum}  onChange={this.handleInputChange}   required/>
-				<DropDown name = "LatitudeUnits"   type = {Location.units.location.latitude.type}  options={Location.units.location.latitude.enum}/>      
-  				<br></br><br></br>
+				<select name = "LatitudeUnits" value={Location.units.location.latitude.default} onChange={this.handleInputChange} >
+			      {Location.units.location.latitude.enum.map(o => <option value={o.value}>{o}</option>)}
+			    </select>				
+				<br></br><br></br>
 			</Panel>
 			<br></br><br></br>
 			<label style = {{color: 'rgb(128, 0, 0)',  fontSize:18, fontFamily: 'Rockwell', width:180}}> Model Name </label>
